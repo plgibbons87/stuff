15e0ac720f62e34085b20e57bf8059b41b11388a
Pat Gibbons <pat.gibbons@hp.com>
2019-06-05 18:23:50 -0500

    experiment with string ops

--- rpn.d
+++ rpn.d
@@ -1,12 +1,16 @@
 void main()
 {
     import std.stdio, std.string, std.algorithm, std.conv;
+    import std.meta;
+
+    alias staticTuple = AliasSeq!(string, string);
 
 //static here forces the compiler to perform compile time function execution CTFE.
-    static string conv(char c)
+    static string conv(string c)
     {
-        return (c == '^') ? "^^" : to!string(c);
+        return (c == "**") ? "^^" : c;
     }
+
 
     // Reduce the RPN expression using a stack
     readln.split.fold!((stack, op)
@@ -14,10 +18,10 @@ void main()
         switch (op)
         {
             // Generate operator switch cases statically
-            static foreach (c; "+-*/^")
-                case [c]:
+            static foreach (o; ["+", "-", "*", "/", "**"])
+                case o:
                     return stack[0 .. $ - 2] ~
-                        mixin("stack[$ - 2] " ~ conv(c) ~
+                        mixin("stack[$ - 2] " ~ conv(o) ~
                             " stack[$ - 1]");
             default: return stack ~ op.to!real;
         }
