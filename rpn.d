b946078f37830ce69ac7c0844d1419a251916d07
Pat Gibbons <pat.gibbons@hp.com>
2019-06-05 19:47:39 -0500

    use version

--- rpn.d
+++ rpn.d
@@ -1,29 +1,72 @@
 void main()
 {
     import std.stdio, std.string, std.algorithm, std.conv;
-    import std.meta;
 
-    alias staticTuple = AliasSeq!(string, string);
-
-//static here forces the compiler to perform compile time function execution CTFE.
-    static string conv(string c)
+    version(iconJack)
     {
-        return (c == "**") ? "^^" : c;
+        string[] iconJackOperators()
+        {
+            return ["+", "-", "*", "/", "**"];
+        }
+
+        string dOperator(string op)
+        {
+            string [string] map;
+
+            static foreach(s; iconJackOperators())
+            {
+                map[s] = s;
+            }
+            map["**"] = "^^";
+            return map[op];
+        }
+
+        string[] getOperators()
+        {
+            return iconJackOperators();
+        }
+    }
+    else
+    {
+        string[] operators()
+        {
+            return ["+", "-", "*", "/", "^"];
+        }
+        string dOperator(string op)
+        {
+            string [string] map;
+
+            static foreach(s; dOperators())
+            {
+                map[s] = s;
+            }
+            map["^"] = "^^";
+            return map[op];
+
+        }
+        string[] getOperators()
+        {
+            return operators();
+        }
     }
 
-
     // Reduce the RPN expression using a stack
+static if (true)
+{
+    
     readln.split.fold!((stack, op)
     {
         switch (op)
         {
             // Generate operator switch cases statically
-            static foreach (o; ["+", "-", "*", "/", "**"])
+            static foreach (o; getOperators())
                 case o:
                     return stack[0 .. $ - 2] ~
-                        mixin("stack[$ - 2] " ~ conv(o) ~
+                        mixin("stack[$ - 2] " ~ dOperator(o) ~
                             " stack[$ - 1]");
             default: return stack ~ op.to!real;
         }
     })((real[]).init).writeln;
+    }    
+
 }
